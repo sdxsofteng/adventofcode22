4558014ebc1c437d77be93130c58062ff1870de2
sdxsofteng <sdx.cs@protonmail.com>
2022-12-06 16:08:09 -0500

    Day 6 Lol

--- day6/day6.py
+++ day6/day6.py
@@ -1,8 +1,18 @@
+from collections import Counter
+
 import aocd
 
-data = list(aocd.get_data(day=5, year=2022).splitlines())
+data = aocd.get_data(day=6, year=2022)
+
+answer = -1
+
+for i in range (0, len(data) - 14):
+    counter = Counter(data[i:i+14])
+    if len(counter) == 14:
+        answer = i + 14
+        break
 
 
 
-aocd.submit(answer, day=5, year=2022, part="a")
-#aocd.submit(answer, day=5, year=2022, part="b")
+#aocd.submit(answer, day=6, year=2022, part="a")
+aocd.submit(answer, day=6, year=2022, part="b")
